98c2932d38ede78649b31b8e4e8c678882f4dcdc
JohnProphet <87950318+JohnProphet@users.noreply.github.com>
2024-01-10 20:11:24 -0600

    TestTaskThree 11 of 14

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,22 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+//    @Test
+//    public void testSettingJobId() throws  {
+//        Job job = createJob("Web Developer", "LaunchCode", "StL", "Back-end developer", "Java");
+//        String jobString = getJobString(job);
+//        assertNotEquals(jobString, job.toString());
+//    }
+
+
+
 }
