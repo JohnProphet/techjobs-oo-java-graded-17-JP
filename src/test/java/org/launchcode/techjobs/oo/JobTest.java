61abcb1a477a0766bea74cbbc19d3e4b3bb8ffdd
JohnProphet <87950318+JohnProphet@users.noreply.github.com>
2024-01-11 18:17:28 -0600

    Task 5: 29 of 40 pass

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -10,13 +10,23 @@ import static org.junit.Assert.assertNotEquals;
 public class JobTest {
     //TODO: Create your unit tests here
 
-//    @Test
-//    public void testSettingJobId() throws  {
-//        Job job = createJob("Web Developer", "LaunchCode", "StL", "Back-end developer", "Java");
-//        String jobString = getJobString(job);
-//        assertNotEquals(jobString, job.toString());
-//    }
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job("Web Developer", new Employer("LaunchCode"), new Location("StL"), new PositionType("Back-end developer"), new CoreCompetency("Java"));
+        Job job2 = new Job("Web Graphics", new Employer("CodeLaunch"), new Location("LtS"), new PositionType("Front-end developer"), new CoreCompetency("Java"));
+        assertNotEquals(job1, job2);
+    }
 
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+    @Test
+    public void testJobsForEquality() {
+        Job job1 = new Job("Web Graphics", new Employer("CodeLaunch"), new Location("LtS"), new PositionType("Front-end developer"), new CoreCompetency("Java"));
+        Job job2 = new Job("Web Graphics", new Employer("CodeLaunch"), new Location("LtS"), new PositionType("Front-end developer"), new CoreCompetency("Java"));
+        assertEquals(job1, job2);
+    }
 
 
 }
